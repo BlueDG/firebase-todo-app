93880b3cda8d34482ecb264cd0c5c29f49beebf7
Guillaume DG <guillaume.dusseuxgicquel@gmail.com>
2020-03-02 00:36:24 +0100

    firebase permission

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,7 @@ const config = {
   apiKey: "AIzaSyBCSq8cFg7TfxNiuCDgih8nqwCddronCFI",
   authDomain: "todo-list-c84ea.firebaseapp.com",
   databaseURL: "https://todo-list-c84ea.firebaseio.com",
-  projectId: "todo-list-c84ea",
-  storageBucket: "todo-list-c84ea.appspot.com",
-  messagingSenderId: "981224174699",
-  appId: "1:981224174699:web:40c8b0690eaa2866482d2e",
-  measurementId: "G-624FZ8M2NF"
+  projectId: "todo-list-c84ea"
 };
 
 firebase.initializeApp(config);
